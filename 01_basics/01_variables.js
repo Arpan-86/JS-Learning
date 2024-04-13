64e2c698d39d3ed17544b340dabdd7de3d31fd31
Arpan-86 <78528249+Arpan-86@users.noreply.github.com>
2024-04-13 11:12:38 +0000

    container changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "arpan1867@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+accountEmail = "arpan@gmail.com"
+accountPassword = "45678"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
