874f879fd84f25aaf24c04047cf4897960fda034
Arpan Majumder <78528249+Arpan-86@users.noreply.github.com>
2024-04-18 15:16:44 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33" 
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggesIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "arpan" => true
+
+
