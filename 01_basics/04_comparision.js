ba376d60c506b6e5d695d4c3398de5931e8c71b3
Arpan Majumder <78528249+Arpan-86@users.noreply.github.com>
2024-04-19 18:42:20 +0000

    04

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === is used to strict check which checking both element and its datatype
+
+console.log("2" === 2);
