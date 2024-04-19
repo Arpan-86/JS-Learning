1b0700b10a245af6f66ffdd09118c96548caeb02
Arpan Majumder <78528249+Arpan-86@users.noreply.github.com>
2024-04-19 11:50:17 +0000

    day 3

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,3 +19,29 @@ console.log(booleanIsLoggesIn)
 // "arpan" => true
 
 
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);       //12
+console.log(1 + "2");       //12
+console.log("1" + 2 + 2);   //122 
+console.log(1 + 2 + "2");   //32
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);    //1
+console.log(+"");      //0
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
